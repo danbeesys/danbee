35698751401a1dd6ae0fedb05a2b8fc6038bffc8
js <wlstjd34@gmail.com>
2023-08-06 16:35:45 +0900

    kakao map api 제거

--- src/direction/Directions.js
+++ src/direction/Directions.js
@@ -3,25 +3,9 @@ import {useEffect} from "react";
 import style from "./Direction.module.css"
 
 function Directions() {
-    useEffect(()=>{
-        var container = document.getElementById('map');
-        var options = {
-            center: new window.kakao.maps.LatLng(37.358864, 127.125568),
-            level: 3
-        };
-        var map = new window.kakao.maps.Map(container, options);
-        var markerPosition  = new window.kakao.maps.LatLng(37.358864, 127.125568);
-        var marker = new window.kakao.maps.Marker({
-            position: markerPosition
-        });
-        marker.setMap(map);
-    }, [])
     return (
         <div>
             <Header />
-            <div className={style.map__box}>
-                <div id="map" className={style.map}></div>
-            </div>
         </div>);
 }
 

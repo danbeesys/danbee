8800fac8344e8cd2f8f1c5d66b805793d5315363
js <wlstjd34@gmail.com>
2023-08-06 17:16:28 +0900

    image change component 기술

--- src/counsel/Counsel.js
+++ src/counsel/Counsel.js
@@ -2,18 +2,13 @@ import Header from "../header/Header"
 import ActivityBox from "./ActivityBox";
 import active from "./active.png"
 import style from "./Counsel.module.css"
+import ChangeViewComponent from "../intro/ChangeViewComponent";
 
 function Counsel() {
     return <div>
         <Header />
         <div className={style.boxes}>
-            <ActivityBox imageSrc={active} text={"활동 설명 글 ~~~~~~~~~~~ ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ"} />
-            <ActivityBox imageSrc={active} text={"활동 설명 글 ~~~~~~~~~~~ ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ"} />
-            <ActivityBox imageSrc={active} text={"활동 설명 글 ~~~~~~~~~~~ ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ"} />
-            <ActivityBox imageSrc={active} text={"활동 설명 글 ~~~~~~~~~~~ ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ"} />
-            <ActivityBox imageSrc={active} text={"활동 설명 글 ~~~~~~~~~~~ ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ"} />
-            <ActivityBox imageSrc={active} text={"활동 설명 글 ~~~~~~~~~~~ ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ"} />
-
+            <ChangeViewComponent></ChangeViewComponent>
         </div>
     </div>
 }
---
+++ src/intro/ChangeViewComponent.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from "react";
+import image1 from "./opening1.PNG"
+import image2 from "./opening2.PNG"
+import image3 from "./opening3.PNG"
+function ChangeViewComponent() {
+    const picture = [image1, image2, image3]
+    const [imageIndex, setImageIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setImageIndex(prev => (
+                prev === picture.length - 1 ? 0 : prev + 1
+            ));
+        }, 5000);
+        return () => clearInterval(intervalId);
+    },[])
+
+
+    return <div className="slideshow-container">
+        <img
+            src={picture[imageIndex]}
+        />
+    </div>;
+}
+export default ChangeViewComponent;

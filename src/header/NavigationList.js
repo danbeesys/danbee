37c82d9c4ec1e93652b0bcc641f6e6521a3976f4
jinseong_oh <jinseong_oh@tmax.co.kr>
2023-08-14 18:51:19 +0900

    Header Navigation의 Mobuile 모드에서 CSS Style을 구분하여 적용하도록 수정

--- src/header/NavigationList.js
+++ src/header/NavigationList.js
@@ -4,7 +4,9 @@ import PropTypes from "prop-types";
 
 function NavigationList({flex, toggle}) {
     let clazz = "";
+    let elem = style.elem;
     if (flex) {
+        elem += ` ${style.elem__flex}`;
         clazz += style.vertical;
         if (toggle) {
             clazz += ` ${style.show}`;
@@ -12,15 +14,16 @@ function NavigationList({flex, toggle}) {
             clazz += ` ${style.hide}`;
         }
     } else {
+        elem += ` ${style.elem__vertical}`;
         clazz += style.horizon;
     }
 
     return <div className={clazz}>
-        <Link to={"/danbee"} className={style.elem}>소개</Link>
-        <Link to={"/adviser"} className={style.elem}>상담사 약력</Link>
-        <Link to={"/process"} className={style.elem}>상담/절차</Link>
-        <Link to={"/psychometry"} className={style.elem}>심리검사</Link>
-        <Link to={"/story"} className={style.elem}>단비 스토리</Link>
+        <Link to={"/danbee"} className={elem}>소개</Link>
+        <Link to={"/adviser"} className={elem}>상담사 약력</Link>
+        <Link to={"/process"} className={elem}>상담/절차</Link>
+        <Link to={"/psychometry"} className={elem}>심리검사</Link>
+        <Link to={"/story"} className={elem}>단비 스토리</Link>
     </div>
 }
 

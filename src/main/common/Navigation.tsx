0be31572b3d0443c2107ff9157f17477aab8acbb
jinseong <wlstjd34@gmail.com>
2024-08-18 19:48:59 +0900

    모바일 지원

--- src/main/common/Navigation.tsx
+++ src/main/common/Navigation.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 import logo from "./logo.png"
 import './Navigation.css';
 
@@ -8,10 +9,11 @@ const Navigation: React.FC = () => {
         <Link to="/danbee"><img src={logo} alt="logo" className='logo' /></Link>
         <ul>
           <Link to="/danbee/main#intro"><li>단비 소개</li></Link>
+          { isMobile ? (<></>) : (<>
           <Link to="/danbee/main#fields"><li>상담 분야</li></Link>
           <Link to="/danbee/main#program"><li>프로그램</li></Link>
           <Link to="/danbee/main#process"><li>상담 절차</li></Link>
-          <Link to="/danbee/main#story"><li>단비 스토리</li></Link>
+          <Link to="/danbee/main#story"><li>단비 스토리</li></Link></>)}
           <Link to="/danbee/diagnosis"><li>자가진단</li></Link>
         </ul>
       </nav>

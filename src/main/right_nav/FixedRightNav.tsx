9f2d2ff49617e90186ab80f4044c7bbd733c9b33
jinseong <wlstjd34@gmail.com>
2024-09-01 23:18:24 +0900

    우측네비 상목 추가

--- src/main/right_nav/FixedRightNav.tsx
+++ src/main/right_nav/FixedRightNav.tsx
@@ -2,6 +2,8 @@ import './FixedRightNav.css';
 import { Fab } from '@mui/material';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import MessageIcon from '@mui/icons-material/Message';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
 import React from 'react';
 
 const FixedRightNav: React.FC = () => {
@@ -14,6 +16,14 @@ const FixedRightNav: React.FC = () => {
         <Fab variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}}>
             <MessageIcon sx={{ mr: 1 }} />
             abcdefg
+        </Fab>
+        <Fab variant="extended" size="small" color="primary" sx={{width:150, height:50, mt:1, mb:1}}>
+            <AssignmentIndIcon sx={{ mr: 1 }} />
+            개인정보 활용 동의서
+        </Fab>
+        <Fab variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}}>
+            <ContentPasteIcon sx={{ mr: 1 }} />
+            상담 신청서
         </Fab>
     </nav>
   );

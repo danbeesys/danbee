6639e99eaf527ff570fc184093400b1e1fcf9230
jinseong <wlstjd34@gmail.com>
2024-10-26 22:01:20 +0900

    Special 프로그램 내용 변경

--- src/main/cprogram/Program.tsx
+++ src/main/cprogram/Program.tsx
@@ -6,6 +6,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { Height } from '@mui/icons-material';
 
 const Program: React.FC = () => {
     return (
@@ -15,80 +16,52 @@ const Program: React.FC = () => {
             <h2>Special 프로그램</h2>
           </div>
           <div className='program-body-right'>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ArrowDropDownIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-              >
-                <Typography sx={{
-                      color: 'success.dark',
-                      display: 'inline',
-                      fontWeight: 'bold',
-                      mx: 0.5,
-                      fontSize: 14,
-                    }}>학업상담 (일반학생·수험생 상담)</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography sx={{
-                  fontWeight: 'fontWeightLight'
-                }}>
+            <div>
+              <h3>학업상담 (일반학생·수험생 상담)</h3>
                     <p>학업계획수립과 실행에 대한 코칭</p>
                     <p>집중력을 높이기 위한 심리 안정화 작업 실시</p>
                     <p>불안을 낮추기 위한 내면 강화 프로그램 적용</p>
                     <p>학업 동기화를 위한 심상 기법 사용</p>
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ArrowDropDownIcon />}
-                aria-controls="panel2-content"
-                id="panel2-header"
-              >
-                <Typography sx={{
-                      color: 'success.dark',
-                      display: 'inline',
-                      fontWeight: 'bold',
-                      mx: 0.5,
-                      fontSize: 14,
-                    }}>그림책 테라피</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography sx={{
-                  fontWeight: 'fontWeightLight'
-                }}>
+
+                  <div style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                  }}>
+                  <img style={{
+                      width: '100px',
+                      height: '100px',
+                    }} src={book} />
+                  </div>
+            </div>
+            <div>
+              <h3>그림책 테라피</h3>
                 <p>그림책은 전 연령이 읽을 수 있으며, 그림과 글로 마음을 이야기하기에 알맞습니다. </p>
                 <p>자신의 이야기를 하기 어려워하는 아동, 청소년들의 경우 그림책을 사용하여 상담을 하기도 합니다. </p>
                 <p> 그림책은 내면에 담긴 이야기를 부담없이 표현하는데 알맞은 도구입니다.</p>
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ArrowDropDownIcon />}
-                aria-controls="panel3-content"
-                id="panel3-header"
-              >
-                <Typography sx={{
-                      color: 'success.dark',
-                      display: 'inline',
-                      fontWeight: 'bold',
-                      mx: 0.5,
-                      fontSize: 14,
-                    }}>놀이 치료</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography sx={{
-                  fontWeight: 'fontWeightLight'
-                }}>
-                <p>놀이치료는 아동이 놀이를 하며 무의식으로 갈등을 표현하게 하고,</p>
-                <p>스스로 선택한 놀이에서 창조적인 방법으로 문제 해결을 할 수 있게 도와줍니다.</p>
-                <p>그리고 치료사와 놀이를 매개로 자유롭게 의사소통을 하고, 억압된 감정을 해소하며, 감정을 정화합니다.</p>
-                <p>인형, 게임, 찰흙, 미술재료, 손인형 등의 도구를 사용합니다.</p>
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                  }}>
+                  <img style={{
+                      width: '100px',
+                      height: '100px',
+                    }} src={book} />
+                  </div>
+            </div>
+            <div>
+              <h3>놀이 치료</h3>
+                <p>놀이치료는 아동이 놀이를 하며 무의식으로 갈등을 표현하게 하고, 스스로 선택한 놀이에서 창조적인 방법으로 문제 해결을 할 수 있게 도와줍니다.</p>
+                <p>그리고 치료사와 놀이를 매개로 자유롭게 의사소통을 하고, 억압된 감정을 해소하며, 감정을 정화합니다. 인형, 게임, 찰흙, 미술재료, 손인형 등의 도구를 사용합니다.</p>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                  }}>
+                  <img style={{
+                      width: '100px',
+                      height: '100px',
+                    }} src={book} />
+                  </div>
+            </div>
           </div>
         </div>
     </section>

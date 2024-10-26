159167e1767dc8fdde010945bbd17a5d94231ff7
jinseong <wlstjd34@gmail.com>
2024-10-27 01:18:59 +0900

    상담소 모습도 왼쪽으로 이동

--- src/main/counselPhoto/ShowPhotos.tsx
+++ src/main/counselPhoto/ShowPhotos.tsx
@@ -3,12 +3,13 @@ import ImageIntroduce from "../ainrto_pic/ImageIntroduce";
 
 const ShowPhotos: React.FC = () => {
     return <div className="image_introduce">
+        <h2 className="image_introduce_right">
+            상담소   
+            모습
+        </h2>
         <div className="image_introduce_left">
             <ImageIntroduce />
         </div>
-        <h2 className="image_introduce_right">
-            상담소 모습
-        </h2>
     </div>
 }
 

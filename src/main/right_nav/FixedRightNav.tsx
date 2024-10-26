a231e913f14089e00a8b9445386c9ebf83f24237
jinseong <wlstjd34@gmail.com>
2024-10-26 18:21:14 +0900

    우측 하단 네비] 모바일 모드 시 글자 미표기

--- src/main/right_nav/FixedRightNav.tsx
+++ src/main/right_nav/FixedRightNav.tsx
@@ -5,25 +5,26 @@ import MessageIcon from '@mui/icons-material/Message';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 
 const FixedRightNav: React.FC = () => {
   return (
     <nav className="fix_nav">
-        <Fab variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}} href='tel:PHONE'>
+        <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}} href='tel:PHONE'>
             <LocalPhoneIcon sx={{ mr: 1 }} />
-            031-714-1714
+            {isMobile ? "" : "031-714-1714"}
         </Fab>
-        <Fab variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}} href='https://open.kakao.com/o/syUYt8Qg'>
+        <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}} href='https://open.kakao.com/o/syUYt8Qg'>
             <MessageIcon sx={{ mr: 1 }} />
-            오픈 채팅
+            {isMobile ? "" : "카카오톡"}
         </Fab>
-        <Fab variant="extended" size="small" color="primary" sx={{width:150, height:50, mt:1, mb:1}}>
+        <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:150, height:50, mt:1, mb:1}}>
             <AssignmentIndIcon sx={{ mr: 1 }} />
-            개인정보 활용 동의서
+            {isMobile ? "" : "개인정보 활용 동의서"}
         </Fab>
-        <Fab variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}}>
+        <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}}>
             <ContentPasteIcon sx={{ mr: 1 }} />
-            상담 신청
+            {isMobile ? "" : "상담 신청"}
         </Fab>
     </nav>
   );

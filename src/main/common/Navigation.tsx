7abddf30399ca7cc9093b745abd23cf727117c20
jinseong <wlstjd34@gmail.com>
2024-10-27 02:47:40 +0900

    hash-link에 대한 처리를 react 라이브러리로 변경

--- src/main/common/Navigation.tsx
+++ src/main/common/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
 import { isMobile } from "react-device-detect";
 import logo from "./logo.png"
 import './Navigation.css';
@@ -6,15 +6,15 @@ import './Navigation.css';
 const Navigation: React.FC = () => {
     return (
       <nav className="main-nav">
-        <Link to="/danbee"><img src={logo} alt="logo" className='logo' /></Link>
+        <HashLink to="/danbee"><img src={logo} alt="logo" className='logo' /></HashLink>
         <ul>
-          <Link to="/danbee/main#intro"><li>단비 소개</li></Link>
+          <HashLink to="/danbee/main#intro"><li>단비 소개</li></HashLink>
           { isMobile ? (<></>) : (<>
-          <Link to="/danbee/main#fields"><li>상담 분야</li></Link>
-          <Link to="/danbee/main#program"><li>Special 프로그램</li></Link>
-          <Link to="/danbee/main#process"><li>상담 절차</li></Link>
-          <Link to="/danbee/main#story"><li>단비 스토리</li></Link></>)}
-          <Link to="/danbee/diagnosis"><li>자가진단</li></Link>
+          <HashLink to="/danbee/main#fields"><li>상담 분야</li></HashLink>
+          <HashLink to="/danbee/main#program"><li>Special 프로그램</li></HashLink>
+          <HashLink to="/danbee/main#process"><li>상담 절차</li></HashLink>
+          <HashLink to="/danbee/main#story"><li>단비 스토리</li></HashLink></>)}
+          <HashLink to="/danbee/diagnosis"><li>자가진단</li></HashLink>
         </ul>
       </nav>
     );

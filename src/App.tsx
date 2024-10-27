9e835e13a38a1823bce16eb5df2fe89661036759
jinseong <wlstjd34@gmail.com>
2024-10-27 09:39:40 +0900

    hash router로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Banner from './welcompage/Banner';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import NotFound from './main/common/NotFound';
-import Swiper from './main/common/Swiper';
 import MainView from './main/MainView';
 import SelfDiagnosis from './main/eselfdiagnosis/SelfDiagnosis';
 import ParentDiagnosis from './main/eselfdiagnosis/parent/ParentDiagnosis';
@@ -14,18 +13,18 @@ import SmartPhoneDiagnosisResult from './main/eselfdiagnosis/smartphone/SmartPho
 const App: React.FC = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
-        <Route path ="/danbee" element={<Banner />}></Route>
-        <Route path ="/danbee/main" element={<MainView />}></Route>
-        <Route path ="/danbee/diagnosis" element={<SelfDiagnosis />}></Route>
-        <Route path ="/danbee/diagnosis/smartphone" element={<SmartPhoneDiagnosis />}></Route>
-        <Route path ="/danbee/diagnosis/smartphone/result" element={<SmartPhoneDiagnosisResult />}></Route>
-        <Route path ="/danbee/diagnosis/parent" element={<ParentDiagnosis />}></Route>
-        <Route path ="/danbee/diagnosis/parent/result" element={<ParentDiagnosisResult />}></Route>
+        <Route path ="/" element={<Banner />}></Route>
+        <Route path ="/main" element={<MainView />}></Route>
+        <Route path ="/diagnosis" element={<SelfDiagnosis />}></Route>
+        <Route path ="/diagnosis/smartphone" element={<SmartPhoneDiagnosis />}></Route>
+        <Route path ="/diagnosis/smartphone/result" element={<SmartPhoneDiagnosisResult />}></Route>
+        <Route path ="/diagnosis/parent" element={<ParentDiagnosis />}></Route>
+        <Route path ="/diagnosis/parent/result" element={<ParentDiagnosisResult />}></Route>
         <Route path ="*" element={<NotFound />}></Route>
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
--- src/main/common/Navigation.tsx
+++ src/main/common/Navigation.tsx
@@ -6,15 +6,15 @@ import './Navigation.css';
 const Navigation: React.FC = () => {
     return (
       <nav className="main-nav">
-        <HashLink to="/danbee"><img src={logo} alt="logo" className='logo' /></HashLink>
+        <HashLink to="/"><img src={logo} alt="logo" className='logo' /></HashLink>
         <ul>
-          <HashLink to="/danbee/main#intro"><li>단비 소개</li></HashLink>
+          <HashLink to="/main#intro"><li>단비 소개</li></HashLink>
           { isMobile ? (<></>) : (<>
-          <HashLink to="/danbee/main#fields"><li>상담 분야</li></HashLink>
-          <HashLink to="/danbee/main#program"><li>Special 프로그램</li></HashLink>
-          <HashLink to="/danbee/main#process"><li>상담 절차</li></HashLink>
-          <HashLink to="/danbee/main#story"><li>단비 스토리</li></HashLink></>)}
-          <HashLink to="/danbee/diagnosis"><li>자가진단</li></HashLink>
+          <HashLink to="/main#fields"><li>상담 분야</li></HashLink>
+          <HashLink to="/main#program"><li>Special 프로그램</li></HashLink>
+          <HashLink to="/main#process"><li>상담 절차</li></HashLink>
+          <HashLink to="/main#story"><li>단비 스토리</li></HashLink></>)}
+          <HashLink to="/diagnosis"><li>자가진단</li></HashLink>
         </ul>
       </nav>
     );

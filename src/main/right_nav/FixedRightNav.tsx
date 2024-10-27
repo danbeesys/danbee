bee5076ec0c9af2bd9085f286ea6abbcecfd859c
jinseong <wlstjd34@gmail.com>
2024-10-27 12:04:12 +0900

    우하단 네비 mobile css 적용

--- src/main/right_nav/FixedRightNav.tsx
+++ src/main/right_nav/FixedRightNav.tsx
@@ -8,26 +8,29 @@ import React from 'react';
 import { isMobile } from 'react-device-detect';
 
 const FixedRightNav: React.FC = () => {
-  return (
-    <nav className="fix_nav">
-        <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}} href='tel:PHONE'>
-            <LocalPhoneIcon sx={{ mr: 1 }} />
-            {isMobile ? "" : "031-714-1714"}
-        </Fab>
-        <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}} href='https://open.kakao.com/o/syUYt8Qg' target='_blank'>
-            <MessageIcon sx={{ mr: 1 }} />
-            {isMobile ? "" : "카카오톡"}
-        </Fab>
-        <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}} href='https://blog.naver.com/sephy6371' target='_blank'>
-            <AssignmentIndIcon sx={{ mr: 1 }} />
-            {isMobile ? "" : "블로그"}
-        </Fab>
-        <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:150, mt:1, mb:1}}>
-            <ContentPasteIcon sx={{ mr: 1 }} />
-            {isMobile ? "" : "상담 신청"}
-        </Fab>
-    </nav>
-  );
+    const widthSize = isMobile ? 40 : 150;
+    const iconMr = isMobile ? 0 : 1;
+
+    return (
+      <nav className="fix_nav">
+          <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:widthSize, mt:1, mb:1}} href='tel:PHONE'>
+              <LocalPhoneIcon sx={{ mr: iconMr }} />
+              {isMobile ? "" : "031-714-1714"}
+          </Fab>
+          <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:widthSize, mt:1, mb:1}} href='https://open.kakao.com/o/syUYt8Qg' target='_blank'>
+              <MessageIcon sx={{ mr: iconMr }} />
+              {isMobile ? "" : "카카오톡"}
+          </Fab>
+          <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:widthSize, mt:1, mb:1}} href='https://blog.naver.com/sephy6371' target='_blank'>
+              <AssignmentIndIcon sx={{ mr: iconMr }} />
+              {isMobile ? "" : "블로그"}
+          </Fab>
+          <Fab className='elem' variant="extended" size="small" color="primary" sx={{width:widthSize, mt:1, mb:1}}>
+              <ContentPasteIcon sx={{ mr: iconMr }} />
+              {isMobile ? "" : "상담 신청"}
+          </Fab>
+      </nav>
+    );
 };
 
 export default FixedRightNav;

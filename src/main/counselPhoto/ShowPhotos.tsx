750e6b156202ab4dc87cf28feb532cd3ec60863f
jinseong <wlstjd34@gmail.com>
2024-10-27 11:11:02 +0900

    상담소 모습 mobild css 적용

--- src/main/counselPhoto/ShowPhotos.tsx
+++ src/main/counselPhoto/ShowPhotos.tsx
@@ -4,8 +4,7 @@ import ImageIntroduce from "../ainrto_pic/ImageIntroduce";
 const ShowPhotos: React.FC = () => {
     return <div className="image_introduce">
         <h2 className="image_introduce_right">
-            상담소   
-            모습
+            상담소 모습
         </h2>
         <div className="image_introduce_left">
             <ImageIntroduce />
